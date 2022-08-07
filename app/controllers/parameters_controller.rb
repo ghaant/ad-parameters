20360e43f75e3d07276083809f33c5bd517f6014
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2022-08-07 14:11:42 +0200

    Remvoe temporary output from the controller.

--- app/controllers/parameters_controller.rb
+++ app/controllers/parameters_controller.rb
@@ -3,7 +3,6 @@ class ParametersController < ApplicationController
     Placement::Seq::Create.new(Hash.from_xml(request.raw_post)['Configuration']).run
     render json: 'Processed', status: 200
   rescue REXML::ParseException
-    puts 'hello'
     render json: 'An input XML format is incorrect', status: 422
   end
 end
