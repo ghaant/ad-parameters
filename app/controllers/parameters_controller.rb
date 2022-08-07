dd14116a1f3d19a704f62de1fc9b995e9c2c5496
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2022-08-07 18:23:29 +0200

    Refactor ParametersController.

--- app/controllers/parameters_controller.rb
+++ app/controllers/parameters_controller.rb
@@ -1,8 +1,18 @@
 class ParametersController < ApplicationController
+  before_action :xml_to_hash, only: [:parse_xml]
+
   def parse_xml
-    print Placement::Seq::Create.new(Hash.from_xml(request.raw_post)).run
-    render json: 'Processed', status: 200
+    print Placement::Seq::Create.new(@xml_hash).run
+    render json: 'Processed.', status: 200
+  end
+
+  private
+
+  def xml_to_hash
+    return render json: 'An input XML is empty.', status: 422 if request.raw_post.blank?
+
+    @xml_hash = Hash.from_xml(request.raw_post)
   rescue REXML::ParseException
-    render json: 'An input XML format is incorrect', status: 422
+    render json: 'An input XML format is incorrect.', status: 422
   end
 end
