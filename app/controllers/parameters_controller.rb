cc136a498359d81ec5c21ac0fb48fedc16672241
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2022-08-07 14:08:47 +0200

    Handle wrong XML format in the controller.

--- app/controllers/parameters_controller.rb
+++ app/controllers/parameters_controller.rb
@@ -1,8 +1,9 @@
 class ParametersController < ApplicationController
   def parse_xml
-    #Hash.from_xml(File.read('public/dashboard_configuration/2022-08-04.xml'))
     Placement::Seq::Create.new(Hash.from_xml(request.raw_post)['Configuration']).run
-
-    render status: 200
+    render json: 'Processed', status: 200
+  rescue REXML::ParseException
+    puts 'hello'
+    render json: 'An input XML format is incorrect', status: 422
   end
 end
